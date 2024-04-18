aa5cd74c70ae85eb94b018d5012400860b15cc04
rohanwebuwu <rohanshah6982@gmail.com>
2024-04-18 19:51:23 +0530

    finally

--- dynamic_html.py
+++ dynamic_html.py
@@ -1,20 +1,19 @@
 from jinja2 import Environment, FileSystemLoader
 env = Environment(loader=FileSystemLoader('.'))
 data = {
-    'title': 'Confirm your email address',
     'property_name': 'Spacious Studio in City Center',
     'num_bed': 1,
     'num_bath': 1,
+    'phone': '123-456-7890',
     'price': 1500,
     'agent_name': 'Rohan Shah',
-    'requested_timeslot': {
-        '2022-12-01': ['09:00', '10:00'],
-        '2022-12-02': ['11:00', '14:00'],
-    },
+    'requested_timeslot': [{
+        '2022-12-01': '10:00 AM-2:00 PM',
+        '2022-12-02': '11:00 AM-3:00 PM'
+    }],
     'email_id': 'rohanshah6982@gmail.com',
-    'requested_timeslot': '2:00pm',
     'requested_timeslot_date': '26th of March',
-    'status': 'Awaiting Confirmation',
+    'status': 'Declined by agent',
     'reason': 'reason given by the landlord',
     'confirmed_time': '10:00 AM',
     'lockbox_info': 'Code: 12345, Location: Front door',
